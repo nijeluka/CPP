30f7933c7ed7d417bcbd42346e984b5d2761fad1
nijeluka <95843118+nijeluka@users.noreply.github.com>
2021-12-16 09:16:31 +0100

    Add files via upload

---
+++ njeluka1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+		int x=4;
+		int y=10;
+
+		int a=x+y;
+		int b=x-y;
+		int c=x*y;
+		int d=x/y;
+		int e=x%y;
+
+		printf("Zbrajanje = &d/n",a);
+		printf("Oduzimanje = %d/n",b);
+		printf("Množenje = %d/n",c);
+		printf("Dijeljenje = %d/n",d);
+		printf("Ostatak cjelobrojnog dijeljenja = %d",e);
+				return 0;
+}
+
+
