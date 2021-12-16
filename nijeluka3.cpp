5509ecff8f5acca949dd5350c0f94bab3349f2ab
nijeluka <95843118+nijeluka@users.noreply.github.com>
2021-12-16 10:07:51 +0100

    Add files via upload

---
+++ nijeluka3.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main()
+	{
+		
+		printf(" 4 > 6-> %d.\n", (4>6));
+		printf(" 10 < 20 -> %d.\n", (10<20));
+		printf(" 10 <= 12  -> %d.\n", (10<=12));
+		printf(" 21 >= 10 -> %d.\n", (21>=10));
+		printf(" 10 != 11 -> %d.\n", (10!=11));
+		printf(" 15 == 11 -> %d.\n", (15==11));
+		
+		
+	return 0;	
+	}
