c0f4ec979205448cd663d49d9b9dc8ddc55cf471
nijeluka <95843118+nijeluka@users.noreply.github.com>
2021-12-16 09:26:56 +0100

    Add files via upload

---
+++ nijeluka2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main()
+{
+	int a;
+	int b;
+	int c;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	
+	printf("Povrsina trokuta = %d\n", (a*b)/2);
+}
