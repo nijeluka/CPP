3c189e8230a8e8e5df7d7bbe83d300ecfdbb8386
nijeluka <95843118+nijeluka@users.noreply.github.com>
2022-02-09 18:16:09 +0100

    Add files via upload

---
+++ Ispis6Znamenki.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int x;
+
+    printf("Unesi neki 6-znamenkasti broj:");
+    scanf("%d", &x);
+
+    int x1=x%10;
+    int x2=(x/10)%10;
+    int x3=(x/100)%10;
+    int x4=(x/1000)%10;
+    int x5=(x/10000)%10;
+    int x6=(x/100000)%10;
+    printf("%d",x1+x2+x3+x4+x5+x6);
+
+
+
+
+}
+
