4dcd631c342f1e7e1f7e4d2e9cafb8eb6289e131
nijeluka <95843118+nijeluka@users.noreply.github.com>
2022-03-24 08:18:40 +0100

    Add files via upload

---
+++ petlja.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int n=-1,i,x;
+
+	
+	
+	printf("\tUnesi broj\n");
+	scanf("%d",&n);
+	
+		if(n<0)
+		printf("Error");
+	
+	
+	for(i=n;i>0;i--){
+	for(x=n;x>0;x--) 
+		printf(".");
+	printf("\n");
+}
+}
+	
+	
