89dd1596d12e2883d41135a047b761526b8c3683
nijeluka <95843118+nijeluka@users.noreply.github.com>
2022-04-07 07:41:26 +0200

    Add files via upload

---
+++ barcode.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(){
+	
+	
+	int n,m,sum=0;
+
+	
+	printf("\tUnesi n\t\n");
+	scanf("%d",&n);
+	
+	while(n){
+	
+
+	m=m*10+n%10;
+	n/=10;
+}
+	
+	while(m){
+	scanf("%d",&n);
+	sum+=n*(m%10);
+	m/=10;
+	
+	}
+ 	printf("%d \n",sum);
+	return 0;
+	
+	
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
