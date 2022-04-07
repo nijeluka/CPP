255a3b999e5d67fce8099fddac7523371fcb0849
nijeluka <95843118+nijeluka@users.noreply.github.com>
2022-04-07 08:43:05 +0200

    Add files via upload

---
+++ BubnjeviStan.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+	int n,k,min=15;
+
+
+	printf("\tunesi kat na kojem se Ivan nalazi \t\n");
+	scanf("%d",&n);
+	
+	printf("\tKoliko stanra ima u zgradin\t\n");
+	scanf("%d",&k);
+	
+	while(k){
+	scanf("%d",k);
+	min=abs(n-i)<min?abs(n-i);
+	
+	printf("nema toliko katova u stanu");
+}
+
+	
+	printf("smetace mu %d \n",min);
+	
+	
+}
