1cb2b63384e1ffce9ece44b5429e8672c730d3f2
nijeluka <95843118+nijeluka@users.noreply.github.com>
2022-05-12 08:41:12 +0200

    Add files via upload

---
+++ Tutel-Akmačić.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i;
+	int a[5]={16,-3,4,2,10};
+	
+	
+	
+ 	for(i=4;i>=0;i--){
+	 
+	 printf("%d\n",a[i]); 
+ 	
+}
+	return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
