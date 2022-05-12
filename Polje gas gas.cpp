aaea6fdaebb41a8ddf58ea9999c0f28c360414ea
nijeluka <95843118+nijeluka@users.noreply.github.com>
2022-05-12 08:25:27 +0200

    Add files via upload

---
+++ Polje gas gas.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i;
+	int x[6];
+	
+	
+	
+ 	for(i=0;i<6;i++){  //upis podataka
+ 		
+ 		printf("Unesi broj:");
+ 		scanf("%d",&x[i]);
+	 }
+	
+	 printf("\n");
+	 
+	 for (i=0;i<6;i++){   //ispis podataka
+	 	
+	 	printf("%d\n",x[i]);
+	 }
+	 return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
