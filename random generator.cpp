958bcf466bc5f21a43eccbd8b044d79f86b03567
nijeluka <95843118+nijeluka@users.noreply.github.com>
2022-11-04 10:21:40 +0100

    Add files via upload

---
+++ random generator.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+int main(){
+time_t t;
+srand((unsigned)time(&t));
+
+printf("%d"rand()%broj);	//vamo di je %broj tu ide do broja koji hoces da bude max broj za random generator
+
+
+
+
+}
+
